e6928f3f17cbae45f0704caecfcd31e781d7a271
Zach Caceres <zcaceres@users.noreply.github.com>
2024-08-17 13:09:12 -0600

    check for api key before commit

---
+++ scripts/check-for-api-key.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+if grep -r --exclude-dir=node_modules --exclude=check-for-api-key.sh --exclude=.gitignore -l 'sk-' .; then
+    echo "Error: Files containing 'sk-' pattern found."
+fi
